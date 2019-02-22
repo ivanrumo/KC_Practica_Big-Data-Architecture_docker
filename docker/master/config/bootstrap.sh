3f5e8080413d303f819419c7e8d1667b4f89edc0
ivan <ivan@myhappyhour.me>
2019-02-22 16:54:05 +0100

    small changes

--- docker/master/config/bootstrap.sh
+++ docker/master/config/bootstrap.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
-
-
 rm /tmp/*.pid
 
 
 service ssh start
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh 
-
-sleep 30
-
-hdfs dfs -mkdir -p input
-
-# put input files to HDFS
-hdfs dfs -put /data/* input
-
-# run wordcount 
-hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/sources/hadoop-mapreduce-examples-2.7.7-sources.jar org.apache.hadoop.examples.WordCount input/user_ids_answers output
-
-if [[ $1 == "-d" ]]; then
-#    while true; do sleep 1000; done
-fi
-
-if [[ $1 == "-bash" ]]; then
-#    /bin/bash
-fi
